a5586812d54d51426c3bbe0fdbadb4b841f72d19
N. A. Ronald <nronald@swin.edu.au>
2023-08-08 15:57:48 +1000

    Fixed missing statement in test.

--- app/src/androidTest/java/au/edu/swin/sdmd/w06_myfirstintent_2023/MainActivityTest.kt
+++ app/src/androidTest/java/au/edu/swin/sdmd/w06_myfirstintent_2023/MainActivityTest.kt
@@ -5,13 +5,14 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,6 +22,9 @@ import org.junit.Assert.*
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun test_checkDetailImageAppears() {
