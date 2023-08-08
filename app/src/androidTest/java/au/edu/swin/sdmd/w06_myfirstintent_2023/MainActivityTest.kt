c13f99349e58d178d2eec991aa930b0973341ecd
N. A. Ronald <nronald@swin.edu.au>
2023-08-08 15:44:25 +1000

    First commit.

---
+++ app/src/androidTest/java/au/edu/swin/sdmd/w06_myfirstintent_2023/MainActivityTest.kt
@@ -0,0 +1,36 @@
+package au.edu.swin.sdmd.w06_myfirstintent_2023
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+
+    @Test
+    fun test_checkDetailImageAppears() {
+
+        onView(
+            withId(R.id.mainImage)
+        ).perform(click())
+
+        onView(
+            withId(R.id.detailImage)
+        ).check(matches(isDisplayed()))
+    }
+}
